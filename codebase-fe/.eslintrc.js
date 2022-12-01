0108323b9c6d543e3ca09d99fcbf96de540d8e3d
Dark-jin <chch4lee1@naver.com>
2022-12-01 19:48:12 +0900

    .DS_Store removed

---
+++ codebase-fe/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'on',
+    '@typescript-eslint/explicit-function-return-type': 'on',
+    '@typescript-eslint/explicit-module-boundary-types': 'on',
+    '@typescript-eslint/no-explicit-any': 'on',
+    'unused-imports/no-unused-imports-ts': 'on',
+  },
+};
